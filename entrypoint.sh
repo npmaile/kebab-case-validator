74024d47ca20007effed228342560019245d5b43
Nathaniel P Maile <npmaile@gmail.com>
2020-03-25 15:39:34 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 EXCLUSIONS=${INPUT_EXCLUSIONS:-'^\.git.*$'}
 REGEX=${INPUT_FIND_REGEX:-'.*'}
 echo "checking files:"
-if find . -regex "${REGEX}" -not -regex "${EXCLUSIONS}" | grep -e -v '^(.*\/)*(((\.?([a-z]|[0-9])+-?)+)|([A-Z]+(\.md)?))$' ; then
+if find . -regex "${REGEX}" -not -regex "${EXCLUSIONS}" | grep -ev '^(.*\/)*(((\.?([a-z]|[0-9])+-?)+)|([A-Z]+(\.md)?))$' ; then
 	return 1
 fi
 
